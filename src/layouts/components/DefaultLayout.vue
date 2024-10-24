44c498a7a0d585038ad20d3d387d6797fba49fda
AzaZLO <MaloyMeee@yandex.ru>
2024-10-24 17:02:19 +0300

    add github in sidebar

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -273,16 +273,15 @@ dayjs()
           </div>
         </a>
         <a
-          v-if="showDiscord"
-          href="https://discord.com/invite/CmjYVSr6GW"
+          href="https://github.com/g7AzaZLO"
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:discord" class="text-xl mr-2" />
+          <Icon icon="mdi:github" class="text-xl mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
-            Discord
+            Github
           </div>
         </a>
         <a
