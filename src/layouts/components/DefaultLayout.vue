7e07e2777837a66a6ac026c5533a724f101108ac
AzaZLO <MaloyMeee@yandex.ru>
2024-10-24 17:12:01 +0300

    fix icon in sidebar

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -313,7 +313,7 @@ dayjs()
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:teletype" class="text-xl mr-2" />
+          <Icon icon="mdi:note" class="text-xl mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
@@ -325,7 +325,7 @@ dayjs()
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:notion" class="text-xl mr-2" />
+          <Icon icon="mdi:alpha-n-box" class="text-xl mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
