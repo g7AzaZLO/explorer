824799221c3b4aa3e32b26ba3f18db5e53eb2fa3
AzaZLO <MaloyMeee@yandex.ru>
2024-10-25 00:42:11 +0300

    try fix text color

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -38,31 +38,33 @@ const chainStore = useBlockchain()
 
 </script>
 <template>
-  <div class="">
-        <div class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2">
-          <h1 class="text-[#22E6FF] text-3xl md:!text-6xl font-bold">
-            {{ $t('pages.title') }}
-          </h1>
-        </div>
-    <div class="text-center text-base">
-      <p class="mb-1">
-        {{ $t('pages.slogan') }}
-      </p>
-    </div>
-    <div
-      v-if="dashboard.status !== LoadingStatus.Loaded"
-      class="flex justify-center"
-    >
-      <progress class="progress progress-info w-80 h-1"></progress>
-    </div>
+    <div class="">
+      <div class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2">
+        <h1 class="text-[#22E6FF] text-3xl md:!text-6xl font-bold">
+          {{ $t('pages.title') }}
+        </h1>
+      </div>
 
-    <div v-if="featured.length>0" class="text-center text-base mt-6 text-primary">
-      <h2 class="mb-6"> Featured Blockchains 🔥 </h2>
-    </div>
+      <div class="text-center text-base text-gray-800 dark:text-gray-200">
+        <p class="mb-1">
+          {{ $t('pages.slogan') }}
+        </p>
+      </div>
 
-    <div v-if="featured.length>0"
-      class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5"
-    >
+      <div
+        v-if="dashboard.status !== LoadingStatus.Loaded"
+        class="flex justify-center"
+      >
+        <progress class="progress progress-info w-80 h-1"></progress>
+      </div>
+
+      <div v-if="featured.length>0" class="text-center text-base mt-6 text-primary">
+        <h2 class="mb-6"> Featured Blockchains 🔥 </h2>
+      </div>
+
+      <div v-if="featured.length>0"
+        class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5"
+      >
     <ChainSummary
         v-for="(chain, index) in featured"
         :key="index"
