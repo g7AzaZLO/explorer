760c240de1be44540111a8c1c02cc5a8fdb9fd37
AzaZLO <MaloyMeee@yandex.ru>
2024-10-24 16:49:43 +0300

    add chat icon to links

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -290,11 +290,11 @@ dayjs()
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:frequently-asked-questions" class="text-xl mr-2" />
+          <Icon icon="mdi:chat" class="text-xl mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
-            FAQ
+            Chat
           </div>
         </a>
       </div>
